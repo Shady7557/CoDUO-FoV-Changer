3d9ff2425a3385f832d1b6ad8eaeccbff4225bfc
Shady7557 <matt_950@hotmail.com>
2015-11-05 16:40:58 -0600

    -Commit AdvSettingsForm

---
+++ CoDUO FoV Changer/AdvSettingsForm.Designer.vb
@@ -0,0 +1,132 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class AdvSettingsForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.SaveRestartAppButton = New System.Windows.Forms.Button()
+        Me.FirstRunCheckBox = New System.Windows.Forms.CheckBox()
+        Me.DisableUpdateTimerCheck = New System.Windows.Forms.CheckBox()
+        Me.CancelButton = New System.Windows.Forms.Button()
+        Me.OpenConfigButton = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+        Me.SuspendLayout()
+        '
+        'SaveRestartAppButton
+        '
+        Me.SaveRestartAppButton.Location = New System.Drawing.Point(12, 58)
+        Me.SaveRestartAppButton.Name = "SaveRestartAppButton"
+        Me.SaveRestartAppButton.Size = New System.Drawing.Size(107, 23)
+        Me.SaveRestartAppButton.TabIndex = 0
+        Me.SaveRestartAppButton.Text = "Save And Restart"
+        Me.SaveRestartAppButton.UseVisualStyleBackColor = True
+        '
+        'FirstRunCheckBox
+        '
+        Me.FirstRunCheckBox.AutoSize = True
+        Me.FirstRunCheckBox.Location = New System.Drawing.Point(12, 12)
+        Me.FirstRunCheckBox.Name = "FirstRunCheckBox"
+        Me.FirstRunCheckBox.Size = New System.Drawing.Size(68, 17)
+        Me.FirstRunCheckBox.TabIndex = 3
+        Me.FirstRunCheckBox.Text = "First Run"
+        Me.ToolTip1.SetToolTip(Me.FirstRunCheckBox, "Tell the program to act as if the user has never ran" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "this program before.")
+        Me.FirstRunCheckBox.UseVisualStyleBackColor = True
+        '
+        'DisableUpdateTimerCheck
+        '
+        Me.DisableUpdateTimerCheck.AutoSize = True
+        Me.DisableUpdateTimerCheck.Location = New System.Drawing.Point(12, 35)
+        Me.DisableUpdateTimerCheck.Name = "DisableUpdateTimerCheck"
+        Me.DisableUpdateTimerCheck.Size = New System.Drawing.Size(128, 17)
+        Me.DisableUpdateTimerCheck.TabIndex = 4
+        Me.DisableUpdateTimerCheck.Text = "Disable Update Timer"
+        Me.ToolTip1.SetToolTip(Me.DisableUpdateTimerCheck, "Disables periodically checking for updates every few minutes " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "while the program " & _
+        "is running.")
+        Me.DisableUpdateTimerCheck.UseVisualStyleBackColor = True
+        '
+        'CancelButton
+        '
+        Me.CancelButton.Location = New System.Drawing.Point(12, 116)
+        Me.CancelButton.Name = "CancelButton"
+        Me.CancelButton.Size = New System.Drawing.Size(107, 22)
+        Me.CancelButton.TabIndex = 11
+        Me.CancelButton.Text = "Cancel"
+        Me.CancelButton.UseVisualStyleBackColor = True
+        '
+        'OpenConfigButton
+        '
+        Me.OpenConfigButton.Location = New System.Drawing.Point(12, 87)
+        Me.OpenConfigButton.Name = "OpenConfigButton"
+        Me.OpenConfigButton.Size = New System.Drawing.Size(107, 23)
+        Me.OpenConfigButton.TabIndex = 12
+        Me.OpenConfigButton.Text = "Open Config"
+        Me.OpenConfigButton.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(225, 289)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(154, 34)
+        Me.Button2.TabIndex = 14
+        Me.Button2.Text = "Search for UO (Potentially Lengthy)"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(222, 191)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(142, 81)
+        Me.TextBox1.TabIndex = 15
+        '
+        'AdvSettingsForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(135, 144)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.OpenConfigButton)
+        Me.Controls.Add(Me.CancelButton)
+        Me.Controls.Add(Me.DisableUpdateTimerCheck)
+        Me.Controls.Add(Me.FirstRunCheckBox)
+        Me.Controls.Add(Me.SaveRestartAppButton)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.Name = "AdvSettingsForm"
+        Me.ShowIcon = False
+        Me.Text = "Advanced Settings"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents SaveRestartAppButton As System.Windows.Forms.Button
+    Friend WithEvents FirstRunCheckBox As System.Windows.Forms.CheckBox
+    Friend WithEvents DisableUpdateTimerCheck As System.Windows.Forms.CheckBox
+    Friend WithEvents CancelButton As System.Windows.Forms.Button
+    Friend WithEvents OpenConfigButton As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents ToolTip1 As System.Windows.Forms.ToolTip
+End Class
---
+++ CoDUO FoV Changer/AdvSettingsForm.vb
@@ -0,0 +1,311 @@
+﻿Public Class AdvSettingsForm
+    Public userpth As String = System.Environment.GetEnvironmentVariable("userprofile")
+    Public appdata As String = System.Environment.GetEnvironmentVariable("appdata") & "\"
+    Dim ini As New IniFile(appdata & "CoD UO FoV Changer\options.ini")
+    Dim whatami As String
+    Dim conf As System.Threading.Thread
+    Dim search As System.Threading.Thread
+    Dim foundUO As Boolean = False
+    Dim uoDirS As String
+    Public iscontext As Boolean = False
+    Private Sub getConf()
+
+        Try
+            If Not My.Computer.FileSystem.FileExists(Application.StartupPath & "\CoDUO FoV Changer.exe.config") Then
+                Dim request3 As System.Net.HttpWebRequest = System.Net.HttpWebRequest.Create("https://docs.google.com/uc?export=download&id=0B0nCag_Hp76zOUp3MnBrRXd6M0U")
+                Dim response3 As System.Net.HttpWebResponse = request3.GetResponse()
+
+                Dim sr3 As System.IO.StreamReader = New System.IO.StreamReader(response3.GetResponseStream())
+
+                Dim read As String = sr3.ReadToEnd()
+
+                '  Dim rn As New Random
+                whatami = read
+                My.Computer.FileSystem.WriteAllText(Application.StartupPath & "\CoDUO FoV Changer.exe.config", whatami, False)
+            Else
+                Dim srr As New System.IO.StreamReader(Application.StartupPath & "\CoDUO FoV Changer.exe.config")
+                whatami = srr.ReadToEnd
+                srr.Close()
+            End If
+
+
+
+        Catch ex As Exception
+            MsgBox("Unable to fetch exe.config! " & vbNewLine & ex.Message, MsgBoxStyle.Critical)
+        End Try
+    End Sub
+    Private Sub Form5_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        conf = New Threading.Thread(AddressOf getConf)
+        conf.Start()
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles SaveRestartAppButton.Click
+        Try
+            Dim fog As String = ""
+            'Dim hotkeys As String
+            Dim disableupdatetimer As String = ""
+            Dim firstrun As String = ""
+
+            If FirstRunCheckBox.Checked = True Then
+                firstrun = "Yes"
+            Else
+                firstrun = "No"
+
+            End If
+
+            If DisableUpdateTimerCheck.Checked = True Then
+                disableupdatetimer = "Yes"
+            Else
+                disableupdatetimer = "No"
+            End If
+
+            '  ini.WriteValue("HotKeys", "Enabled", hotkeys)
+            ini.WriteValue("Extras", "Fog", fog)
+            ini.WriteValue("Tweaks", "DisableUpdateTimer", disableupdatetimer)
+            ini.WriteValue("Extras", "FirstRun", firstrun)
+
+            Application.Restart()
+        Catch ex As Exception
+            MsgBox("Failed to set config! Reason: " & ex.Message, MsgBoxStyle.Critical)
+        End Try
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs)
+        Me.Close()
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs)
+        MsgBox("Disabled, may not work... this bug is being fixed. In the mean time if you care enough you can manually set it. It seems to work 50% of the time.", MsgBoxStyle.Information)
+    End Sub
+
+    Private Sub CheckBox4_CheckedChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button3_Click_1(sender As Object, e As EventArgs) Handles CancelButton.Click
+        If iscontext = False Then
+            SettingsForm.Visible = True
+            Me.Visible = False
+        Else
+            Me.Visible = False
+        End If
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles OpenConfigButton.Click
+        Process.Start(appdata & "CoDUO FoV Changer")
+    End Sub
+    Private Function searchuo4() As Boolean
+        Dim i As Integer = 0
+
+        'Target Directory
+        Dim directory = "D:\"
+
+        'Searches directory and it's subdirectories for all files, which "*" stands for
+        'Say for example you only want to search for jpeg files... then change "*" to "*.jpg"  
+        For Each filename As String In IO.Directory.GetFiles(directory, "*", IO.SearchOption.AllDirectories)
+
+            'The next line of code gets only file extensions from searched directories and subdirectories
+            Dim fName As String = IO.Path.GetExtension(filename)
+
+            If fName = ".suo" Then
+
+                'Skips to next iteration of Loop, ignoring files with .suo extension 
+                Continue For
+
+            Else
+                If fName = ".ini" Then
+
+                    'Skips to next iteration of Loop, ignoring files with .ini extension
+                    Continue For
+
+                Else
+
+                    'Your code here above count function
+                    'The below counter only displays the final count after all files have been processed
+                    If filename.Contains("CoDUOMP") Then
+                        MessageBox.Show("success")
+                        MessageBox.Show(filename)
+                        uoDirS = filename
+                        Return True
+                    End If
+
+                    i = i + 1
+                    ' TextBox1.Text = Convert.ToString(i)
+
+
+                End If
+            End If
+        Next
+    End Function
+    Private Function searchuo3() As Boolean
+        Dim i As Integer = 0
+
+        'Target Directory
+        Dim directory = "C:\"
+
+        'Searches directory and it's subdirectories for all files, which "*" stands for
+        'Say for example you only want to search for jpeg files... then change "*" to "*.jpg"  
+        For Each filename As String In IO.Directory.GetFiles(directory, "*", IO.SearchOption.AllDirectories)
+
+            'The next line of code gets only file extensions from searched directories and subdirectories
+            Dim fName As String = IO.Path.GetExtension(filename)
+
+            If fName = ".suo" Then
+
+                'Skips to next iteration of Loop, ignoring files with .suo extension 
+                Continue For
+
+            Else
+                If fName = ".ini" Then
+
+                    'Skips to next iteration of Loop, ignoring files with .ini extension
+                    Continue For
+
+                Else
+
+                    'Your code here above count function
+                    'The below counter only displays the final count after all files have been processed
+                    If filename.Contains("CoDUOMP") Then
+                        MessageBox.Show("success")
+                        MessageBox.Show(filename)
+                        uoDirS = filename
+                        Return True
+                    End If
+
+                    i = i + 1
+                    ' TextBox1.Text = Convert.ToString(i)
+
+
+                End If
+            End If
+        Next
+    End Function
+    Private Function searchuo2() As Boolean
+        Dim i As Integer = 0
+
+        'Target Directory
+        Dim directory = "C:\Program Files"
+
+        'Searches directory and it's subdirectories for all files, which "*" stands for
+        'Say for example you only want to search for jpeg files... then change "*" to "*.jpg"  
+        For Each filename As String In IO.Directory.GetFiles(directory, "*", IO.SearchOption.AllDirectories)
+
+            'The next line of code gets only file extensions from searched directories and subdirectories
+            Dim fName As String = IO.Path.GetExtension(filename)
+
+            If fName = ".suo" Then
+
+                'Skips to next iteration of Loop, ignoring files with .suo extension 
+                Continue For
+
+            Else
+                If fName = ".ini" Then
+
+                    'Skips to next iteration of Loop, ignoring files with .ini extension
+                    Continue For
+
+                Else
+
+                    'Your code here above count function
+                    'The below counter only displays the final count after all files have been processed
+                    If filename.Contains("CoDUOMP") Then
+                        MessageBox.Show("success")
+                        MessageBox.Show(filename)
+                        uoDirS = filename
+                        Return True
+                    End If
+
+                    i = i + 1
+                    ' TextBox1.Text = Convert.ToString(i)
+
+
+                End If
+            End If
+        Next
+    End Function
+    Private Function searchuo() As Boolean
+        Dim i As Integer = 0
+
+
+        Dim directory = "C:\Program Files (x86)"
+        Dim directory2 = "C:\Program Files"
+        Dim directory3 = Nothing
+
+
+        For Each filename As String In IO.Directory.GetFiles(directory, "*", IO.SearchOption.AllDirectories)
+
+
+            Dim fName As String = IO.Path.GetExtension(filename)
+
+            If fName = ".a" Then
+
+                'test code
+                Continue For
+
+            Else
+
+                If filename.Contains("CoDUOMP") And fName.Contains(".exe") Then
+                    MessageBox.Show("success")
+                    MessageBox.Show(filename)
+                    uoDirS = filename
+                    Return True
+                End If
+
+                i = i + 1
+                ' TextBox1.Text = Convert.ToString(i)
+
+
+            End If
+        Next
+    End Function
+    Private Sub Checkconnection()
+        If searchuo() = False Then
+            foundUO = False
+        Else
+            foundUO = True
+        End If
+        '      If isathread = True And isDev = True Then
+        '    MsgBox("THREADED")
+        'End If
+        AccessLabel()
+    End Sub
+    Private Sub AccessLabel()
+        Try
+            If Me.InvokeRequired Then
+                Me.Invoke(New MethodInvoker(AddressOf AccessLabel))
+            Else
+
+                If foundUO = False Then
+                    TextBox1.Text = "not found"
+                Else
+                    TextBox1.Text = uoDirS
+                    Dim yesno = MessageBox.Show(uoDirS & " Found! Does this directory look correct? If you press no, we will continue to search.", Application.ProductName, MessageBoxButtons.YesNoCancel, MessageBoxIcon.Information)
+                    If yesno = MsgBoxResult.Yes Then
+                        MessageBox.Show("HOLY SHIT")
+                    End If
+                End If
+            End If
+        Catch ex As Exception
+            MsgBox("A critical error has occured: " & ex.Message, MsgBoxStyle.Critical)
+        End Try
+    End Sub
+
+    Private Sub Button2_Click_1(sender As Object, e As EventArgs) Handles Button2.Click
+        If Not Debugger.IsAttached = True Then
+            MessageBox.Show("debugger not attached, returning...")
+            Return
+        End If
+        search = New Threading.Thread(AddressOf Me.Checkconnection)
+        search.Start()
+    End Sub
+
+    Private Sub CheckBox5_CheckedChanged(sender As Object, e As EventArgs)
+        If Not Debugger.IsAttached = True Then
+            MessageBox.Show("debugger not attached, returning...")
+            Return
+        End If
+    End Sub
+End Class
