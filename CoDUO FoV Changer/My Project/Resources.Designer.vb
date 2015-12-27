1575bd915a20d06f83edd8782ba1ac00084249ee
Matt <matt_950@hotmail.com>
2015-12-26 21:00:02 -0600

    Removed outdated updater Removed Loading.png

--- CoDUO FoV Changer/My Project/Resources.Designer.vb
+++ CoDUO FoV Changer/My Project/Resources.Designer.vb
@@ -89,15 +89,5 @@ Namespace My.Resources
                 Return CType(obj,Byte())
             End Get
         End Property
-        
-        '''<summary>
-        '''  Looks up a localized resource of type System.Drawing.Bitmap.
-        '''</summary>
-        Friend ReadOnly Property Loading() As System.Drawing.Bitmap
-            Get
-                Dim obj As Object = ResourceManager.GetObject("Loading", resourceCulture)
-                Return CType(obj,System.Drawing.Bitmap)
-            End Get
-        End Property
     End Module
 End Namespace
