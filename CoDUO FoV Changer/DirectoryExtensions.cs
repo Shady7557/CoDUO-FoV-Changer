2998a45a1f0b9a28ef06192f773ea1174f3a876d
Shady7557 <matt_950@hotmail.com>
2020-04-28 14:43:55 -0500

    Removed license that was used in a different app created by me, and removed unused commented code

--- CoDUO FoV Changer/DirectoryExtensions.cs
+++ CoDUO FoV Changer/DirectoryExtensions.cs
@@ -7,30 +7,6 @@ using System.Text;
 
 namespace DirectoryExtensions
 {
-    /*/
-    MIT License
-
-    Copyright (c) 2018, 2019, PRISM.
-
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-    of this software and associated documentation files (the "Software"), to deal
-    in the Software without restriction, including without limitation the rights
-    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    copies of the Software, and to permit persons to whom the Software is
-    furnished to do so, subject to the following conditions:
-
-    The above copyright notice and this permission notice shall be included in
-    all copies or substantial portions of the Software.
-
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-    THE SOFTWARE.
-    /*/
-
     public static class DirectoryExtension
     {
         [DllImport("Kernel32.dll")]
@@ -204,29 +180,5 @@ namespace DirectoryExtensions
                 }
             }
         }
-
-        /*/
-        public static string GetProcessPath(Process process)
-        {
-            if (process == null || process.HasExited) return string.Empty;
-            var wmiQueryString = "SELECT ProcessId, ExecutablePath, CommandLine FROM Win32_Process";
-            using (var searcher = new ManagementObjectSearcher(wmiQueryString))
-            using (var results = searcher.Get())
-            {
-                var query = from p in Process.GetProcesses() join mo in results.Cast<ManagementObject>() on p.Id equals (int)(uint)mo["ProcessId"]
-                            select new
-                            {
-                                Process = p,
-                                Path = (string)mo["ExecutablePath"],
-                                CommandLine = (string)mo["CommandLine"],
-                            };
-
-                foreach (var item in query)
-                {
-                    if (item?.Process?.Id == process.Id) return item.Path;                    
-                }
-            }
-            return string.Empty;
-        }/*/
     }
 }
